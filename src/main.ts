80cf22373e3fc29958c7ac0e3baef33c370e996c
Aadi Dapurkar <aadidapurkar04@gmail.com>
2025-09-26 18:40:16 +1000

    minor - comments

--- src/main.ts
+++ src/main.ts
@@ -32,9 +32,13 @@ const setBackgroundColours : (k : KnowledgeGraph, is : string[]) => void = (k : 
 
 // Main code
 helpDesc.style.visibility = "hidden"
+
+// Display knowledge in linear format
 const kParas = convertKnowledgeToHTMLParagraphs(knowledge)
 kParas.map((p) => knowledgeContainer?.appendChild(p))
 
+// Create a stream for every time the user clicks on a piece of knowlege, mapping it to:
+//                                                                        an object of the unsure knowledge + its parents
 const kClick$ = merge(
   ...knowledge.map(k => 
     fromEvent(document.getElementById(k.id)!, 'click').pipe(
@@ -43,11 +47,16 @@ const kClick$ = merge(
   )
 );
 
+// Update view, update description and highlight prerequisite knowledge if applicable
 kClick$.subscribe((ids) => {
+
+  // Prerequisite knowledge exists
   if(ids.parents) {
     setBackgroundColours(knowledge, ids.parents)
     helpDesc.style.visibility = "visible"
     helpDesc.innerHTML = `Unsure about <strong> ${ids.child}</strong>? <br> Have a look at its highlighted prerequisites: <strong>${ids.parents}</strong>`
+  
+  // Prerequisite knowledge doesn't exist
   } else {
     setBackgroundColours(knowledge, [])
     helpDesc.style.visibility = "visible"
